92065d915e6d8d57bfcb48fd3943a97935c31c60
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-03 22:48:09 +0700

    add get by account holder for company

--- src/models/company.js
+++ src/models/company.js
@@ -11,6 +11,34 @@ module.exports = {
     })
   },
 
+  getCompanyByAccountHolderModel: (searchKey, searchValue, limit, offset, callback) => {
+    db.query(`SELECT 
+    c.companyID, 
+    ac.accountID, 
+    ac.account_name,
+    c.company_name,
+    c.company_position,
+    c.company_latitude,
+    c.company_longitude,
+    c.company_type,
+    c.company_detail,
+    c.company_linkedin,
+    c.company_instagram,
+    c.company_facebook,
+    c.company_image,
+    c.updatedAt
+    FROM company as c
+    INNER JOIN account as ac
+    ON ac.accountID = c.accountID
+    WHERE ${searchKey} LIKE '%${searchValue}%' LIMIT ${limit} OFFSET ${offset}`, (err, result, fields) => {
+      if (!err) {
+        callback(result)
+      } else {
+        callback(err)
+      }
+    })
+  },
+
   getCompanyByIDModel: (companyID) => {
     return new Promise((resolve, reject) => {
       db.query(`SELECT * FROM company WHERE companyID = ${companyID}`, (err, result, fields) => {
